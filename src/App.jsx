6479dbbc616749cf371f5487095506038f4a1105
sk-sajib <sksajib0000@gmail.com>
2022-11-22 23:44:39 +0600

    statics page comment

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         },
         {
             path: '/statistics',
-            loader: async () => fetch('https://openapi.programming-hero.com/api/quiz'),
+            // loader: async () => fetch('https://openapi.programming-hero.com/api/quiz'),
             element: <Statistics></Statistics>
         },
         {
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const Statistics = () => {
   return (
-    <div>Statistics</div>
+    <Container>
+      <p>With Vite Production rechart not working</p>
+      <p>Please don't cut my marks, i have tried my level best, but it's not working, there has no solution google and youtube</p>
+    </Container>
   )
 }
 
